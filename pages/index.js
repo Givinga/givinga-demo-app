45436b61f7c2d3bf726bdf1c88eb30c9ec3e83e9
Sawyer Billings <sawpaw19@gmail.com>
2021-01-14 17:23:29 -0800

    copy change

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,8 @@ export default function Home({ user }) {
             <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto text-justify">
               Welcome to the Givinga Demo App. This open source application
               provides you with all the tools you need to implement Givinga into
-              your own application.
+              your own application. All donations and account fundings are in
+              TEST mode, you will not be charged.
             </p>
             <a
               class="mr-5 bg-primary text-black font-bold py-2 px-6 rounded-md text-white"
